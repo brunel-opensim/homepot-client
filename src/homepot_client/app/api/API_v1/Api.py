46fe41c9c97037971f167a4785de64a4f2c78213
Ramkumar <ram.k@stellartechnologies.ae>
2025-09-29 18:57:37 +0530

    (feature/re-structure-src-dir)
    UI dashboard api module created

--- src/homepot_client/app/api/API_v1/Api.py
+++ src/homepot_client/app/api/API_v1/Api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from .Endpoints import HealthEndpoint
+from .Endpoints import HealthEndpoint, UIEndpoint
 
 api_v1_router = APIRouter()
 
 api_v1_router.include_router(HealthEndpoint.router, prefix="/health", tags=["Health"])
+api_v1_router.include_router(UIEndpoint.router, prefix="/ui", tags=["UI"])
