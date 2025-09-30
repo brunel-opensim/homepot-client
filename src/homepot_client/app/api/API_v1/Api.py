7621cc2ab62d5ae494ee7a38b5b378a1259b708b
Ramkumar <ram.k@stellartechnologies.ae>
2025-09-30 11:26:57 +0530

    (feature/re-structure-src-dir)
    add a client apis as seperate module

--- src/homepot_client/app/api/API_v1/Api.py
+++ src/homepot_client/app/api/API_v1/Api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from .Endpoints import HealthEndpoint, UIEndpoint
+from .Endpoints import HealthEndpoint, UIEndpoint, ClientEndpoint
 
 api_v1_router = APIRouter()
 
 api_v1_router.include_router(HealthEndpoint.router, prefix="/health", tags=["Health"])
 api_v1_router.include_router(UIEndpoint.router, prefix="/ui", tags=["UI"])
+api_v1_router.include_router(ClientEndpoint.router, prefix="/client", tags=["Client"])
