4d5e83ffb9bf87c95321458706e3740f66d7474f
Ramkumar <ram.k@stellartechnologies.ae>
2025-10-03 12:42:10 +0530

    (feature/re-structure-src-dir)
    add device and job module

--- src/homepot_client/app/api/API_v1/Api.py
+++ src/homepot_client/app/api/API_v1/Api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .Endpoints import HealthEndpoint, UIEndpoint, ClientEndpoint, SitesEndpoint
+from .Endpoints import HealthEndpoint, UIEndpoint, ClientEndpoint, SitesEndpoint, DevicesEndpoints, JobsEndpoints
 
 api_v1_router = APIRouter()
 
@@ -7,3 +7,5 @@ api_v1_router.include_router(HealthEndpoint.router, prefix="/health", tags=["Hea
 api_v1_router.include_router(UIEndpoint.router, prefix="/ui", tags=["UI"])
 api_v1_router.include_router(ClientEndpoint.router, prefix="/client", tags=["Client"])
 api_v1_router.include_router(SitesEndpoint.router, prefix="/sites", tags=["Sites"])
+api_v1_router.include_router(DevicesEndpoints.router, prefix="/devices", tags=["Devices"])
+api_v1_router.include_router(JobsEndpoints.router, prefix="/jobs", tags=["Jobs"])
