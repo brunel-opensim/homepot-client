4d329291213090c9fede81639efcf5d667a3f1cf
Radhakrishnan <radhakrishnan.g@redblox.io>
2025-10-06 18:45:07 +0530

    (feature/re-structure-src-dir)
    add agents api in re strcture dir

--- src/homepot_client/app/api/API_v1/Api.py
+++ src/homepot_client/app/api/API_v1/Api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .Endpoints import HealthEndpoint, UIEndpoint, ClientEndpoint, SitesEndpoint, DevicesEndpoints, JobsEndpoints
+from .Endpoints import HealthEndpoint, UIEndpoint, ClientEndpoint, SitesEndpoint, DevicesEndpoints, JobsEndpoints, AgentsEndpoints
 
 api_v1_router = APIRouter()
 
@@ -9,3 +9,4 @@ api_v1_router.include_router(ClientEndpoint.router, prefix="/client", tags=["Cli
 api_v1_router.include_router(SitesEndpoint.router, prefix="/sites", tags=["Sites"])
 api_v1_router.include_router(DevicesEndpoints.router, prefix="/devices", tags=["Devices"])
 api_v1_router.include_router(JobsEndpoints.router, prefix="/jobs", tags=["Jobs"])
+api_v1_router.include_router(AgentsEndpoints.router, prefix="/agents", tags=["Agents"])
