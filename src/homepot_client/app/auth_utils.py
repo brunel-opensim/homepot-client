5554204cd921b06fb8cfd9b6db49a299240b1364
radhakrishnan <radhakrishnan@getfudogroup.com>
2025-10-13 14:43:05 +0530

    (feature/re-structure-src-dir)
    fix blank space

--- src/homepot_client/app/auth_utils.py
+++ src/homepot_client/app/auth_utils.py
@@ -59,7 +59,8 @@ def verify_token(
 
 
 def get_current_user(
-        credentials: HTTPAuthorizationCredentials = Depends(security)) -> TokenData:
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+) -> TokenData:
     """Get the current user from the JWT token."""
     token = credentials.credentials
     try:
@@ -76,7 +77,7 @@ def get_current_user(
 
 def require_role(required_role: str) -> Any:
     """Dependency to require a specific user role."""
-    # def role_checker(user=Depends(get_current_user)) -> UserDict:
+
     def role_checker(user: TokenData = Depends(get_current_user)) -> UserDict:
         if user.role != required_role:
             raise HTTPException(
@@ -86,63 +87,3 @@ def require_role(required_role: str) -> Any:
         return {"email": user.email, "role": user.role}
 
     return role_checker
-
-
-# from passlib.context import CryptContext
-# from jose import jwt,JWTError
-# import os
-# from fastapi.security import OAuth2PasswordBearer
-# from fastapi import Depends, HTTPException, status
-# from pydantic import BaseModel
-# from typing import Optional
-
-
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
-# ALGORITHM = "HS256"
-
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-# def hash_password(password: str) -> str:
-#     return pwd_context.hash(password)
-
-# def verify_password(plain_password: str, hashed_password: str) -> bool:
-#     return pwd_context.verify(plain_password, hashed_password)
-
-# def create_access_token(data: dict):
-#     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
-
-
-# class TokenData(BaseModel):
-#     email: Optional[str] = None
-
-# def verify_token(token: str = Depends(oauth2_scheme)) -> TokenData:
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         email: str = payload.get("sub")
-#         if email is None:
-#             raise HTTPException(status_code=401, detail="Invalid token: no email")
-#         return TokenData(email=email)
-#     except JWTError:
-#         raise HTTPException(status_code=401, detail="Invalid token")
-
-# def get_current_user(token: str = Depends(oauth2_scheme)):
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         return {"email": payload.get("sub"), "role": payload.get("role")}
-#     except JWTError:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Invalid or expired token"
-#         )
-
-# def require_role(required_role: str):
-#     def role_checker(user=Depends(get_current_user)):
-#         if user["role"] != required_role:
-#             raise HTTPException(
-#                 status_code=status.HTTP_403_FORBIDDEN,
-#                 detail=f"Requires {required_role} role"
-#             )
-#         return user
-#     return role_checker
