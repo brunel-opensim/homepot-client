6e8fb8670b758fe053f83a93eb4a7ca2b51d4d1f
dharani-getfudogroup <dharani@getfudogroup.com>
2025-11-14 17:59:25 +0530

    Feature/frontend api implementations (#40)
    - Approve PR #40 on GitHub
    - Merge to main
    - Frontend will now display real device data!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,6 +56,14 @@ function App() {
               </ProtectedRoute>
             }
           />
+          <Route
+            path="/site/:siteId/:deviceId"
+            element={
+              <ProtectedRoute>
+                <Device />
+              </ProtectedRoute>
+            }
+          />
 
           {/* 404 - Catch all unmatched routes */}
           <Route path="*" element={<NotFound />} />
