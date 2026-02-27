9c4efb2b1ab5dbd73e0e8dab7c1169ee5c77128d
gowthamraj4045-coder <gowthamraj4045@gmail.com>
2026-02-27 17:46:35 +0530

    Update Api.py

--- backend/src/homepot/app/api/API_v1/Api.py
+++ backend/src/homepot/app/api/API_v1/Api.py
@@ -1,7 +1,7 @@
 """API router managing for all Endpoints in the HomePot system."""
 
 from fastapi import APIRouter
-from agent import agent_router
+from agent.router import agent_router
 
 from .Endpoints import (
     AgentsEndpoints,
