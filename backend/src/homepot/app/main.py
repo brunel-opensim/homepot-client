c7cec4e8afa65b37147674195aa80af8278d0042
gowthamraj4045-coder <gowthamraj4045@gmail.com>
2026-04-17 14:50:20 +0530

    update Device DNA API backend port from 8001 to 8000

--- backend/src/homepot/app/main.py
+++ backend/src/homepot/app/main.py
@@ -96,4 +96,4 @@ app.include_router(api_v1_router, prefix="/api/v1")
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="info")  # nosec B104
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")  # nosec B104
